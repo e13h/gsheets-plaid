cf4de2216a03957227bd0ea7d42d454a571ead92
Evan Smith <evanphilipsmith@gmail.com>
2022-03-21 15:22:08 -0700

    Plaid transaction data transforms

--- plaid_transactions.py
+++ plaid_transactions.py
@@ -11,6 +11,36 @@ from plaid.model.transactions_get_request_options import TransactionsGetRequestO
 from create_sheet import get_spreadsheet_id, gsheets_service
 
 CONFIG = dotenv_values('.env')
+TRANSACTION_COLS = [
+    'transaction_id',
+    'pending_transaction_id',
+    'pending',
+    'account_id',
+    'date',
+    'datetime',
+    'name',
+    'merchant_name',
+    'amount',
+    'iso_currency_code',
+    'unofficial_currency_code',
+    'payment_channel',
+    'category_id',
+    'category',
+    'personal_finance_category',
+    'location',
+]
+ACCOUNT_COLS = [
+    'account_id',
+    'balances',
+    'name',
+    'type',
+    'subtype',
+]
+ITEM_COLS = [
+    'item_id',
+    'institution_id',
+    'consent_expiration_time',
+]
 
 if CONFIG.get('PLAID_ENV') == 'sandbox':
     host = plaid.Environment.Sandbox
@@ -47,10 +77,51 @@ def get_transaction_data(access_token: str, num_days: int = 30):
             options=options
         )
         response = client.transactions_get(request)
-        cols = ['transaction_id', 'account_id', 'merchant_name', 'name', 'amount', 'date']#, 'category']
-        transactions = pd.DataFrame(response.to_dict().get('transactions'))
+        transactions = pd.DataFrame(response.to_dict().get('transactions'))[TRANSACTION_COLS]
+        accounts = pd.DataFrame(response.to_dict().get('accounts'))[ACCOUNT_COLS]
+        item = pd.Series(response.to_dict().get('item'))[ITEM_COLS]
+
+        # Convert datetime to string
+        def fillna_datetime(row: pd.Series) -> datetime:
+            if row.datetime is not None:
+                return row.datetime
+            return datetime.combine(row.date, datetime.min.time())
+        transactions['datetime'] = transactions.apply(fillna_datetime, axis=1)
+        transactions['datetime'] = transactions['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S%Z')
         transactions['date'] = transactions['date'].astype(str)
-        return transactions[cols].to_numpy().tolist()
+
+        # Expand categories
+        categories = (
+            transactions
+            .category
+            .apply(pd.Series)
+            .fillna('')
+            .rename(columns={0: 'category1', 1: 'category2', 2: 'category3'}))
+
+        # Expand location
+        locations = transactions.location.apply(pd.Series)
+
+        # Combine expanded columns
+        transactions = pd.concat(
+            (
+                transactions.drop(columns=['location', 'category']),
+                categories,
+                locations,
+            ), axis=1)
+
+        # Add account info
+        accounts.set_index('account_id', inplace=True)
+        transactions['account_name'] = transactions.account_id.apply(lambda x: accounts.loc[x].get('name'))
+
+        # Add item info
+        transactions['item_id'] = item.item_id
+        transactions['institution_id'] = item.institution_id
+
+        transactions.set_index('transaction_id', drop=False, inplace=True)
+        headers = transactions.columns.tolist()
+        values = transactions.to_numpy().tolist()
+        values.insert(0, headers)
+        return values
     except plaid.ApiException as e:
         print(e)
 
@@ -60,7 +131,7 @@ def fill_gsheet(transactions: list[list]):
     """
     gsheets_service.spreadsheets().values().append(
         spreadsheetId=get_spreadsheet_id(),
-        range='A2:Z',
+        range='A1:AB',
         valueInputOption='USER_ENTERED',
         body={'values': transactions},
     ).execute()
