aa660d8f4000e676394238a6f004f976f5455684
Evan Smith <evanphilipsmith@gmail.com>
2022-07-09 16:45:15 -0700

    Check new web server into version control

--- gsheets_plaid/web-server/static/plaid_link.js
+++ gsheets_plaid/web-server/static/plaid_link.js
@@ -1,10 +1,10 @@
-function run_plaid_link(link_token, port, received_redirect_uri = null) {
+function run_plaid_link(link_token, received_redirect_uri = null) {
     const handler = Plaid.create({
         token: link_token,
         receivedRedirectUri: received_redirect_uri,
         onSuccess: (public_token, metadata) => {
-            window.location.replace(`http://localhost:${port}/success?public_token=${public_token}`);
-            console.log('public_token', public_token);
+            redirected_url = `${window.location.origin}/plaid-link-success?public_token=${public_token}`;
+            window.location.replace(redirected_url);
             console.log('metadata', metadata);
         },
         onLoad: () => {},
