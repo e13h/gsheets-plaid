6b454ca7595fffff53b926e8e8b413dbc44039b8
Evan Smith <evanphilipsmith@gmail.com>
2022-09-14 15:13:21 -0700

    Remove `load_dotenv` from `__main__`

--- gsheets_plaid/__main__.py
+++ gsheets_plaid/__main__.py
@@ -1,13 +1,9 @@
-import os
 import threading
 import webbrowser
 from time import sleep
 
-from dotenv import load_dotenv
-
 from gsheets_plaid.web_server.main import run_web_server
 
-load_dotenv(os.path.join(os.path.dirname(__file__), 'web_server/.env'))
 t = threading.Thread(target=run_web_server, kwargs={'ssl_context': 'adhoc'})
 t.start()
 sleep(1)  # Wait for the server to start
